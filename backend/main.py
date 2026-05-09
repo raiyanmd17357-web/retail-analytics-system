5e9790c8c9c1169f78a3976756eb7c88ea431c8a
Raiyaan MD <raiyanmd17357@gmail.com>
2026-05-09 11:41:55 +0530

    Added FastAPI routes

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,21 @@
-print("Backend Started")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Retail Analytics System Running"}
+
+@app.get("/detect")
+def detect():
+    return {
+        "people_count": 5,
+        "status": "Detection Working"
+    }
+
+@app.get("/analytics")
+def analytics():
+    return {
+        "total_visitors": 120,
+        "peak_hour": "6 PM"
+    }
