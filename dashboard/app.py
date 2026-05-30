4b608105f6fe55446dc26d7882a8bf1f59428000
Raiyaan MD <raiyanmd17357@gmail.com>
2026-05-30 09:47:25 +0530

    updated

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_autorefresh import st_autorefresh
+import plotly.express as px
 
 # Page title
 st.set_page_config(page_title="Retail Analytics Dashboard", layout="wide")
@@ -10,7 +11,20 @@ st.markdown("""
 ### AI-Powered Store Monitoring System
 ---
 """)
+
+st.sidebar.markdown("---")
+
+selected_rows = st.sidebar.slider(
+    "Records to Display",
+    10,
+    100,
+    50
+)
+
+
 st_autorefresh(interval=5000, key="refresh")
+st.success("🟢 System Active | YOLO Detection Running")
+
 
 st.sidebar.title("🔧 Controls")
 
@@ -21,7 +35,7 @@ st.sidebar.markdown("""
 - Refresh: 5 sec
 """)
 
-use_local_db = True  # change to True for local MySQL
+use_local_db = False  # change to True for local MySQL
 
 if use_local_db:
     import mysql.connector
@@ -62,22 +76,64 @@ col3.metric("📦 Objects Detected", latest_objects)
 st.markdown("---")
 st.markdown("### 📋 Recent Detection Data")
 
-st.dataframe(df.head(20), use_container_width=True)
-st.subheader("Visitor Trend")
+st.dataframe(
+    df.head(selected_rows),
+    use_container_width=True
+)
+
+st.markdown("### 📈 Visitor Trend Analysis")
 
 chart_data = df[["timestamp", "people_count"]].tail(20)
 chart_data = chart_data.sort_values(by="timestamp")
 
-st.line_chart(
-    chart_data.set_index("timestamp")
+fig = px.line(
+    chart_data,
+    x="timestamp",
+    y="people_count",
+    title="Visitor Trend Over Time",
+    markers=True
 )
 
-st.subheader("Object Detection Frequency")
+fig.update_layout(
+    xaxis_title="Time",
+    yaxis_title="People Count",
+    height=450
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("### 📊 Object Detection Analytics")
 
 filtered_df = df[df["detected_objects"] != "None"]
 object_counts = filtered_df["detected_objects"].value_counts()
 
-st.bar_chart(object_counts)
+object_df = object_counts.reset_index()
+object_df.columns = ["Object", "Count"]
+
+fig2 = px.bar(
+    object_df,
+    x="Object",
+    y="Count",
+    text="Count",
+    title="Object Detection Frequency"
+)
+
+fig2.update_traces(textposition="outside")
+
+fig3 = px.pie(
+    object_df,
+    names="Object",
+    values="Count",
+    title="Object Distribution"
+)
+
+col_left, col_right = st.columns(2)
+
+with col_left:
+    st.plotly_chart(fig2, use_container_width=True)
+
+with col_right:
+    st.plotly_chart(fig3, use_container_width=True)
 
 total_visitors = df["people_count"].sum()
 
